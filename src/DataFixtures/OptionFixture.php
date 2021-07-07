8db9b8e762800cfab82bd3bb58b6d388c12c8ba6
Aleksey Vinnik <vinniko333@gmail.com>
2021-07-07 16:42:28 +0300

    Create models fixtures commit

---
+++ src/DataFixtures/OptionFixture.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Option;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Traits\WordsGeneratingTrait;
+
+class OptionFixture extends Fixture
+{
+    use WordsGeneratingTrait;
+
+    public function load(ObjectManager $manager)
+    {
+        for($i = 0; $i < 30; $i++){
+            $option = new Option();
+            $option ->setTitle($this->GenerateWord());
+            $manager->persist($option );
+        }
+        $manager->flush();
+    }
+}
---
+++ src/DataFixtures/ProductFixture.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use App\Traits\WordsGeneratingTrait;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ProductFixture extends Fixture
+{
+    use WordsGeneratingTrait;
+
+    public function load(ObjectManager $manager)
+    {
+        for($i = 0; $i < 30; $i++){
+            $product = new Product();
+            $product->setTitle($this->GenerateWord());
+            $product->setPrice(rand(1, 333));
+            $product->setQty(rand(1, 333));
+            $manager->persist($product);
+        }
+        $manager->flush();
+    }
+}
